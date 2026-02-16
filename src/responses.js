2730fd097c65fd026e968b90797bb77f98545dbd
julianalvia422 <jaa2128@g.rit.edu>
2026-02-15 20:56:14 -0500

    Added Body Parse and Post Requests as well as Get and Head Requests for users

---
+++ src/responses.js
@@ -0,0 +1,85 @@
+// object to contain user objects
+const users = {};
+
+// error messages
+const responses = {
+    'notFound' : {
+        message: 'The page you are looking for was not found',
+        id: 'notFound'
+    },
+    'created' : {
+        message: 'Created Successfully'
+    },
+    'badRequest': {
+        message: 'Name and age are both required.',
+        id: 'addUserMissingParams'
+    }
+};
+
+// function to respond with a JSON object
+const respondJSON = (request, response, status, object) => {
+    
+    const content = JSON.stringify(object);
+
+    // Set Headers including the type and length
+    response.writeHead(status, {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(content, 'utf8'),
+    });
+
+    // only write content if it is not a head request
+    if(request.method !== 'HEAD'){
+        response.write(content);
+    }
+    
+    response.end();
+}
+
+// return user object as JSON
+const getUsers = (request, response) => {
+    respondJSON(request, response, 200, users);
+}
+
+const notFound = (request, response) => {
+    respondJSON(request, response, 404, responses['notFound']);
+}
+
+const addUsers = (request, response) => {
+    let isNewUserCreated = false;
+
+    // use JS destructing to easily grab request's body
+    const {name, age} = request.body;
+
+    // make sure that both fields exist otherwise send proper response
+    if(!name || !age){
+        return respondJSON(request, response, 400, responses['badRequest']);
+    }
+
+    // check if the user exists, if it doesn't, create new user
+    if(!users[name]){
+
+        users[name] = {
+            name: name,
+        };
+
+        // new user is created
+        isNewUserCreated = true;
+    }
+
+    // add or update age for this user name
+    users[name].age = age;
+
+    // If a new user was created send 201 response
+    if(isNewUserCreated){
+        return respondJSON(request, response, 201, responses['created']);
+    }
+
+    // If the user existed and was updated, send 204 response with no body
+    return respondJSON(request, response, 204, {});
+}
+
+module.exports = {
+    getUsers,
+    notFound,
+    addUsers
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 // http and handlers
 const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
-//const respondHandler = require('./responses.js');
+const respondHandler = require('./responses.js');
+
+// querystring module for parsing query strings from url
+const query = require('querystring');
 
 // Port
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
@@ -10,7 +13,62 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const urlStruct ={
     '/': htmlHandler.getIndex,
     '/style.css': htmlHandler.getCss,
-    getIndex: htmlHandler.getIndex
+    '/getUsers': respondHandler.getUsers,
+    '/notReal': respondHandler.notFound,
+    '/addUser': respondHandler.addUsers,
+    notFound: respondHandler.notFound
+}
+
+// function to parse and rebuild the request body of a post request
+// handler method is what we call after reassembling the request
+const parseBody = (request, response, handler) => {
+    
+    //body array to store pieces of the request
+    const body = [];
+
+    // event handler for if there is an error, if there is
+    // print to console and send back a 400 error
+    request.on('error', (err) => {
+        console.dir(err);
+        response.statusCode = 400;
+        response.end();
+    });
+
+    // event handler for when we receive a chunk of data
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+
+    // event handler for when the request is finished sending 
+    // and all data is received
+    request.on('end', () => {
+
+        // concatonates the body into a string
+        const bodyString = Buffer.concat(body).toString();
+
+        // grab content type the request is looking for
+        const type = request.headers['content-type'];
+
+        // Then depending on the type set the request body to 
+        // the respective parsed string
+        // NOTE: for http-api-assignment-ii this is unecessary
+        // as all data is JSON but for Project 1 it is important
+        // as we need to support this x-www-form-urlencoded post requests
+        // thus why this particular implementation as it will be used as a template
+        // for Project 1
+        if(type === 'application/x-www-form-urlencoded'){
+            request.body = query.parse(bodyString);
+        } else if (type === 'application/json'){
+            request.body = JSON.parse(bodyString);
+        } else {
+            response.writeHead(400, {'Content-Type': 'application/json'});
+            response.write(JSON.stringify({error: 'invalid data format'}));
+            return response.end();
+        } 
+
+        // lastly once we have the body params we can call the respective handler function
+        handler(request, response);
+    })
 }
 
 const onRequest = (request, response) => {
@@ -18,11 +76,24 @@ const onRequest = (request, response) => {
     const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
     const handler = urlStruct[parsedUrl.pathname];
-    if(handler){
+
+    // first check if this is a POST request AND if there's a handler
+    // method for the url, if it is, parse the body 
+    // and use the respective handler for the url
+    if(request.method === 'POST' && handler){
+        parseBody(request, response, handler);
+    }
+
+    // if it's a different request like a GET or HEAD
+    // simply call the handler method
+    else if(handler){
         handler(request, response);
     } 
-    else{
-        urlStruct.getIndex(request, response);
+
+    // lastly if there is no handler for the url
+    // use the Not Found handler method
+    else {
+        urlStruct.notFound(request, response);
     }
 };
 
